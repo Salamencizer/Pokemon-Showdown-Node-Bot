62a45561fb59417db7c07bede0b9079ab9e39ae3
Spandan7 <spandan.punwatkar@gmail.com>
2016-06-05 08:24:56 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,362 @@
+/*
+	config.js - Configuration File
+*/
+
+/*
+* Connection Details
+*
+* NOTE: Do NOT use "[server].psim.us", that is the client url
+* If you don't know what are the server, port or serverid values
+* run 'node serverconfig.js' to get them
+*
+*/
+
+exports.server = 'showdownsubserver-austin0602.c9users.io';
+
+exports.port = 80;
+
+exports.serverid = 'showdownsubserver-austin0602.c9users.io:80';
+
+exports.autoReconnectDelay = 10 * 1000;
+exports.connectionTimeout = 2 * 60 * 1000;
+
+/*
+* Crash Guard
+*
+* If true, write errors to console instead of crashing
+*/
+
+exports.crashguard = true;
+
+/*
+* Watch Config
+*/
+
+exports.watchconfig = true;
+
+/*
+* Login Details
+*/
+
+exports.nick = 'Rotom The Pokedex';
+
+exports.pass = 'ohkaydude!';
+
+exports.autoReloginDelay = 60 * 1000;
+
+/*
+* Rooms to join
+*/
+
+exports.rooms = ['lobby'];
+/*
+* exports.rooms = 'all'; //For joining all rooms
+* exports.rooms = 'official'; //For joining official rooms
+* exports.rooms = 'public'; //For joining not-official rooms
+* exports.rooms = ['room1', 'room2']; //For joining some rooms
+*/
+
+exports.privateRooms = { //Rooms listed here will be ignored by seen command
+	//privateroomname: true
+};
+
+exports.ignoreRooms = { //Rooms listed here will be ignored by CommandParser (bot is "asleep" in those rooms)
+	//roomid: true
+};
+
+exports.initCmds = ['|/avatar 120']; // Other commands (avatar, blockchallenges, etc)
+
+/*
+* Auth configuration
+*/
+
+exports.exceptions = {spandan:true,ohkaydude:true};
+
+exports.ranks = ['+', '\u2605', '%', '@', '#', '&', '~'];
+
+exports.globalPermissions = {
+	'voice': '+', //Min rank to broadcast in a server
+	'driver': '%', //Min rank to mute. Also min staff rank
+	'moderator': '@', //Min rank to ban
+	'roomowner': '#', //Rank for using room settings commands like set, lang, mod...
+	'admin': '~' //Rank for using global admin commands
+};
+
+/*
+* Commands configuration
+*/
+
+exports.commandTokens = [".","\\","h","H","g","G"]; //Array of valid command characters
+
+exports.defaultPermission = '%';
+
+exports.permissionExceptions = {
+	//command: 'rank'
+	'say': '#',
+	'info': '+',
+	'wall': '%',
+	'autoban': '#',
+	'banword': '#',
+	'joinphrase': '#',
+	'challenge': '%',
+	'searchbattle': '~',
+	'tournament': '@',
+	'games': '#'
+};
+
+exports.botguide = "https://github.com/Salamencizer/Abyssal-Bot/blob/master/commands/README.md";
+
+exports.pmhelp = "Hola mano! I'm a bot. Do .help for a command list. Have a nice day!!"; //When you pm the bot but don't use a command, it replies you this message. Example: "Hi, I'm a bot. Use .help to view a command guide"
+
+/*
+* Language configuration
+*/
+
+exports.language = 'english';
+
+/*
+* Configuration for console messages
+*/
+
+exports.debug = {
+	/* Basic messages - Production Mode */
+	error: true,
+	ok: true,
+	errlog: true,
+	info: true,
+	room: true,
+
+	/* Monitoring */
+	monitor: true,
+	status: true,
+	battle: false,
+
+	/* Debug Mode */
+	debug: false,
+	cmdr: false,
+
+	/* Low Level */
+	recv: false,
+	sent: false
+};
+
+
+/*
+* Configuration for specific
+* commands and features
+*/
+
+/*
+* Moderation
+*/
+
+exports.moderation = {
+	modException: '+', // Min rank for not receive moderation
+
+	allowmute: true,
+	disableModNote: false,
+
+	MOD_CONSTS: {
+		FLOOD_MESSAGE_NUM: 5,
+		FLOOD_PER_MSG_MIN: 500, // this is the minimum time between messages for legitimate spam. It's used to determine what "flooding" is caused by lag
+		FLOOD_MESSAGE_TIME: 6 * 1000,
+
+		MIN_CAPS_LENGTH: 18,
+		MIN_CAPS_PROPORTION: 0.8,
+
+		MAX_STRETCH: 7,
+		MAX_REPEAT: 4
+	},
+
+	values: {
+		'spam-p': 3,
+		'spam': 4,
+		'spam-link': 4,
+		'flood-hard': 3,
+		'flood': 2,
+		'caps': 1,
+		'stretch': 1,
+		'banwords': 2,
+		'inapwords': 2,
+		'servers': 2,
+		'youtube': 2,
+		'spoiler': 2,
+        'replays':0
+	},
+
+	modDefault: {
+		//basic mods
+		'caps': 1,
+		'stretching': 1,
+		'flooding': 1,
+		'spam': 1,
+
+		'bannedwords': 1,
+		'inapropiate': 1,
+
+		//specific mods
+		'spoiler': 0,
+		'youtube': 0,
+		'psservers': 0,
+
+		//multiple infraction
+		'multiple': 1,
+
+		//zero tolerance
+		'zerotol': 1
+	},
+
+	punishments: [
+		"warn",
+		"mute",
+		"hourmute",
+		"roomban"
+	],
+
+	psServersExcepts: {
+		"showdown": 1,
+		"smogtours": 1
+	},
+
+	zeroToleranceDefaultLevel: 'h',
+	zeroToleranceLevels: {
+		'l': {name: 'Low', value: 1},
+		'n': {name: 'Normal', value: 2},
+		'h': {name: 'High', value: 3}
+	}
+};
+
+/*
+* Battles
+*/
+
+exports.aceptAll = true;
+
+exports.maxBattles = 6;
+
+exports.winmsg = ['Good Game Dude', 'Don\'t worry. My team was too good.','The bot pinched your butt...'];
+
+exports.losemsg = ['Dont get happy, I\'m just a bot.','Huh.'];
+
+exports.battleMessages = {
+	'tier': {
+		'self': ['Good Luck and Have Fun Dude!', 'The bot battle begins!','Warning: You shall expect excessive hax!']
+	},
+	'-crit': {
+		'self':['AAAARGH! A CRIT!', 'WTF I hate crits'], //Example: ['lol that hax', 'stop haxing pls']
+		'foe':['Really sorry bout thatit cr', 'Yay! I got a crit!']
+	},
+	'-miss': {
+		'self':['Looks like i have pleased the haxgods', '#poke, you are as blind as Deino.']
+	}
+};
+
+exports.ladderCheckInterval = 10 * 1000;
+
+exports.ladderNumberOfBattles = 1;
+
+exports.formatAliases = {
+	'random': 'Random Battle',
+	'randomdoubles': 'Random Doubles Battle',
+	'randomtriples': 'Random Triples Battle',
+	'doubles': 'Doubles OU',
+	'triples': 'Smogon Triples',
+	'vgc': 'Battle Spot Doubles (VGC 2015)',
+	'vgc2015': 'Battle Spot Doubles (VGC 2015)',
+	'ag': 'Anything Goes',
+	'oras': 'OU',
+	'bw': '[Gen 5] OU',
+	'dpp': '[Gen 4] OU',
+	'adv': '[Gen 3] OU',
+	'gsc': '[Gen 2] OU',
+	'bh' : 'Balanced Hackmons',
+	'rby': '[Gen 1] OU'
+};
+
+/*
+* Tournaments
+*/
+
+exports.tourDefault = {
+	format: 'ou',
+	type: 'elimination',
+	maxUsers: null,
+	timeToStart: 30 * 1000,
+	autodq: 1.5,
+	scoutProtect: false
+};
+
+/*
+* Youtube
+*/
+
+exports.youtube = {
+	enableByDefault: false
+};
+
+/*
+* Chat Logger
+*/
+
+exports.chatLogger = {
+	rooms: [],
+	ignore: {'tournament': ['update', 'updateEnd'], 'formats': true, 'challstr': true, 'updateuser': true, 'queryresponse': true},
+	logIntroMessages: true,
+	ageOfLogs: 7 //in days (max age of logs, 0 to keep logs infinitely)
+};
+
+/*
+exports.logServer = {
+	port: 5400,
+	bindaddress: null,
+	users: {
+		'admin': {
+			name: 'Administrator',
+			pass: 'password',
+			access: {'room1': 1, 'room2': 1}
+		}
+	},
+	rooms: {
+		'room1': {private: true},
+		'room2': {private: true}
+	}
+};
+*/
+
+/*
+* Auto-Invite
+*/
+
+exports.autoInvite = [
+	//{linked: 'public room linked', private: 'private room', rank: '+'}
+];
+
+/*
+* GitHub
+* Read this: https://developer.github.com/webhooks/creating/
+*/
+
+exports.github = {
+	room: 'development',
+	secret: "",
+	port: 3420
+};
+
+/*
+* Groupchats
+*/
+
+exports.groupchats = {};
+
+exports.groupChatTryJoinInterval = 60 * 1000;
+
+/* Test example
+exports.groupchats['groupchat-ecuacion-test'] = {
+	toJoin: ['/join groupchat-ecuacion-test'],
+	onJoin: ['Hi guys!'],
+	onLeave: [],
+	roomAuth: {
+		'+': [/^.*$/]
+	}
+};
+*/
