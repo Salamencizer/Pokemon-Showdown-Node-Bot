32db84c9b9b8981c670867c6b455e615d64db7c6
Spandan7 <spandan.punwatkar@gmail.com>
2016-06-05 14:26:17 +0530

    Update base-basic.js

--- commands/base-basic.js
+++ commands/base-basic.js
@@ -8,7 +8,7 @@ exports.commands = {
 	credits: 'about',
 	bot: 'about',
 	about: function () {
-		this.restrictReply(this.trad('about') + ". " + this.trad('author') + ": " + Settings.package.author.name + ". (" + Settings.package.homepage + ")", 'info');
+		this.restrictReply("I'm a **Pokemon Showdown Bot** written in JavaScript for Node, modified by **Spandan**. Author: Ecuacion. (https://github.com/Spandan/Pokemon-Showdown-Node-Bot)");
 	},
 
 	git: 'github',
